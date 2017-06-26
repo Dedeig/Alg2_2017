9cd5832d801bb5694bbf4eb71569b53b9fec1d2c
Andrey Gobbo <andrey.gobbo@arquivei.com.br>
2017-06-26 19:40:08 -0300

    Limpeza no codigo e mudança no nome do arquivo.c

--- Trabalho_2/convertToAscii.c
+++ Trabalho_2/convertToAscii.c
@@ -18,23 +18,25 @@
 #define	MATRIXM 1 //Parâmetro 'M' definido na especificação para substituir os bits
 #define MATRIXN 2 //Parâmetro 'N' definido na especificação para substituir os bits
 
+//Struct com os dados do header da imagem - tipo, tamanho do arquivo e offset
 typedef struct {
-   unsigned short int type;                 /* Magic identifier            */
-   unsigned int size;                       /* File size in bytes          */
+   unsigned short int type;                 /* Magic identifier                         */
+   unsigned int size;                       /* Tamanho do arquivo, em Bytes             */
    unsigned short int reserved1, reserved2;
-   unsigned int offset;                     /* Offset to image data, bytes */
+   unsigned int offset;                     /* Offset até o começo da imagem, em Bytes  */
 } HEADER;
 
+//struct com os metadados da imagem - tamanho do header, largura/altura da imagem, tipo de compressão, entre outros
 typedef struct {
-   unsigned int size;               /* Header size in bytes      */
-   int width,height;                /* Width and height of image */
-   unsigned short int planes;       /* Number of colour planes   */
-   unsigned short int bits;         /* Bits per pixel            */
-   unsigned int compression;        /* Compression type          */
-   unsigned int imagesize;          /* Image size in bytes       */
-   int xresolution,yresolution;     /* Pixels per meter          */
-   unsigned int ncolours;           /* Number of colours         */
-   unsigned int importantcolours;   /* Important colours         */
+   unsigned int size;               /* Header size in bytes                  */
+   int width,height;                /* Largura e altura da imagem, em pixels */
+   unsigned short int planes;       /* Número de planos de cores             */
+   unsigned short int bits;         /* Bits por pixel                        */
+   unsigned int compression;        /* Tipo de compressão - será descartado  */
+   unsigned int imagesize;          /* Tamanho da imagem - será descartado   */
+   int xresolution,yresolution;     /* Pixels por metro - será descartado    */
+   unsigned int ncolours;           /* Número de cores                       */
+   unsigned int importantcolours;   /* Cores importantes                     */
 } INFOHEADER;
 
 typedef struct {
@@ -87,33 +89,33 @@ int main(int argc,char **argv)
    COLOURINDEX colourindex[256];
    FILE *fptr;
 
-   /* Check arguments */
+   /* Checa o numero de argumentos ao rodar */
    if (argc < 2) {
       fprintf(stderr,"Usage: %s filename\n",argv[0]);
       exit(-1);
    }
 
-   /* Open file */
+   /* Abre o arquivo BMP */
    if ((fptr = fopen(argv[1],"r")) == NULL) {
       fprintf(stderr,"Não foi possível ler o arquivo BMP \"%s\"\n",argv[1]);
       exit(-1);
    }
 
-   /* Ler e checar o header */
+   /* Ler e checar o header e os metadados da imagem */
    ReadUShort(fptr,&header.type,FALSE);
-   //fprintf(stderr,"ID is: %d, should be %d\n",header.type,'M'*256+'B');
    ReadUInt(fptr,&header.size,FALSE);
-   //fprintf(stderr,"File size is %d bytes\n",header.size);
    ReadUShort(fptr,&header.reserved1,FALSE);
    ReadUShort(fptr,&header.reserved2,FALSE);
    ReadUInt(fptr,&header.offset,FALSE);
    fprintf(stderr,"Offset para o começo dos pixels é %d bytes\n",header.offset);
 
+   //Lê os metadados da imagem
    if (fread(&infoheader,sizeof(INFOHEADER),1,fptr) != 1) {
       fprintf(stderr,"Falha ao ler o HEADER de informações\n");
       exit(-1);
    }
 
+   //Inicia a matriz de cores com o tamanho da imagem Largura x Altura com o valor 0
    int matrizCor[infoheader.height][infoheader.width];
    for(i = 0; i < infoheader.height; i++){
    		for (j = 0; j < infoheader.width; j++)
@@ -122,15 +124,10 @@ int main(int argc,char **argv)
    		}
    	}
    fprintf(stderr,"Tamanho da imagem = %d x %d\n",infoheader.width,infoheader.height);
-   //fprintf(stderr,"Number of colour planes is %d\n",infoheader.planes);
    fprintf(stderr,"Bits por pixel %d\n",infoheader.bits);
-   //fprintf(stderr,"Compression type is %d\n",infoheader.compression);
-   //fprintf(stderr,"Number of colours is %d\n",infoheader.ncolours);
-   //fprintf(stderr,"Number of required colours is %d\n",
-   //   infoheader.importantcolours);
 
    /* Read the lookup table if there is one */
-/*  for (i=0;i<255;i++) {
+   /*for (i=0;i<255;i++) {
       colourindex[i].r = rand() % 256;
       colourindex[i].g = rand() % 256;
       colourindex[i].b = rand() % 256;
@@ -163,34 +160,35 @@ int main(int argc,char **argv)
    /* Move a leitura do arquivo para o começo dos bits de pixel - SEEK_SET = header.offset */
    fseek(fptr,header.offset,SEEK_SET);
 
-   /* Lê a imagem */
+   /* Lê a imagem. Faz a leitura de maneira normal, a partir do canto esquerdo superior [0][0]*/
    for (j = 0;j < infoheader.height; j++) {
       for (i = 0;i < infoheader.width; i++) {
 
-      	//Faz a leitura da imagem, salvando em um buffer. A cor é definida pela media das cores RGB lidas
+      //Faz a leitura da imagem, salvando em um buffer. A cor é definida pela media das cores RGB lidas
 		fread(cbuf,1,infoheader.bits/8,fptr);
 		corPixel = ((short)cbuf[2]+(short)cbuf[1]+(short)cbuf[0])/3;
-      	
-      	matrizCor[j][i] = corPixel;
+      
+      //Salva a media RGB na matriz de cores	
+      matrizCor[j][i] = corPixel;
     	}
     }
 
-   //
-   for (i = (infoheader.height-1) ;i >= 0 ; i--) {
-      for (j = 0;j < infoheader.width; j++) {
- 			//printf("%d ", matrizCor[i][j]);
-      		intensidadeCor = (matrizCor[i][j]+matrizCor[i][j+1])/2;
-      		//printf("%d ", intensidadeCor);
- 			if((intensidadeCor >= 1) && (intensidadeCor <= 25))
- 				printf("#");
+   /*Faz o calculo da intesidade de pixel numa matriz MxN e imprime o caracter correspondente
+      A leitura é feita a partir do canto esquerdo INFERIOR de acordo com a 'dica' da especificação. */
+   for (i = (infoheader.height-1) ;i >= 0 ; i--) { //começa da ultima linha 
+      for (j = 0;j < infoheader.width; j++) { //começa do canto esquerdo
+      	intensidadeCor = (matrizCor[i][j]+matrizCor[i][j+1])/2;
+
+ 		   if((intensidadeCor >= 1) && (intensidadeCor <= 25))
+ 			  	printf("#");
  			else if((intensidadeCor >= 26) && (intensidadeCor <= 50))
- 				printf("$");
+ 		      printf("$");
  			else if((intensidadeCor >= 51) && (intensidadeCor <= 75))
- 				printf("0");
+ 			   printf("0");
  			else if((intensidadeCor >= 76) && (intensidadeCor <= 100))
- 				printf("=");
+ 			   printf("=");
  			else if((intensidadeCor >= 101) && (intensidadeCor <= 125))
- 				printf("+");
+ 			   printf("+");
  			else if((intensidadeCor >= 126) && (intensidadeCor <= 150))
  				printf("|");
  			else if((intensidadeCor >= 151) && (intensidadeCor <= 175))
@@ -201,12 +199,10 @@ int main(int argc,char **argv)
  				printf(".");
  			else if((intensidadeCor >= 226) && (intensidadeCor <= 255))
  				printf(" ");
-      	}
-      	printf("\n");
-      }
+        	}
+     	printf("\n");
+   }
    printf("\n");
 
    fclose(fptr);
 }
-
-//'0','$','O','=','+','|','-','^','.',' '
